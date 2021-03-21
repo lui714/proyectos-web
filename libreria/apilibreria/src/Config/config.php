674bb86656e54d8df82f3c6018e017d6d1d0bbcf
lui714 <luiscuravalera20@gmail.com>
2021-03-21 12:15:01 +0100

    configuracion libreria

--- libreria/apilibreria/src/Config/config.php
+++ libreria/apilibreria/src/Config/config.php
@@ -3,9 +3,9 @@ namespace App\Config;
 
 interface config {
     CONST DB_NAME    = "libreria";
-    CONST DB_USER    = "juank";
+    CONST DB_USER    = "luis";
     CONST DB_PORT    = "3306";
-    CONST DB_PASS    = "usuario";
+    CONST DB_PASS    = "P@ssw0rd";
     CONST DB_CHARSET = "utf8";
     CONST DB_HOST    = "localhost";
 }
