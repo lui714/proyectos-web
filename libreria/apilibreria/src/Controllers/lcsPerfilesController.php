d35dd22fa29ae5949a8d6c5f2aa29b88ec3761ca
lui714 <luiscuravalera20@gmail.com>
2021-03-23 13:50:07 +0100

    arriba

---
+++ libreria/apilibreria/src/Controllers/lcsPerfilesController.php
@@ -0,0 +1,43 @@
+<?php
+    namespace App\Controllers;
+    use Psr\Http\Message\ResponseInterface as Response;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use App\Model\lcsPerfilesModel;
+
+    class lcsPerfilesController {
+    
+        public function new(Request  $request, Response $response, $args){
+            $response->getBody()->write("Insertar una nueva Categoria");
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+        }
+
+        public function lcsnew(Request  $request, Response $response, $args){
+            $parametros = $request->getParsedBody();
+           
+            $usuarioid = (int)$parametros['usuarioid'];
+            $nombre = $parametros['nombre'];
+            $apellidos = $parametros['apellidos'];
+            $direccion = $parametros['direccion'];
+            $anioNac = (int)$parametros['anioNac'];
+            $ciudad = $parametros['ciudad'];
+            $valores = array($usuarioid, $nombre, $apellidos, $direccion, $anioNac, $ciudad);
+            $perfiles = lcsPerfilesModel::lcsnew($valores);
+            $perfilesJson = json_encode($perfiles);
+            $response->getBody()->write($perfilesJson);
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+        }
+
+       
+        public function getAll($request, $response, $args){
+            $perfiles = lcsPerfilesController::getALL();
+            $perfilesJson = json_encode($perifles);
+            $response->getBody()->write($perfilesJson);
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+        }
+    }
---
+++ libreria/apilibreria/src/Model/lcsPerfilesModel.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Model;
+use App\Config\DB;
+
+//definimos LibrosModel como una clase estática:
+//no se puede hacer un new, no hay $this, no hay método __contruct()
+class lcsDetallePerfilesModel {
+    private static $table = 'perfiles';
+    private static $DB;
+
+    public static function conexionDB(){
+        lcsDetallePerfilesModel::$DB = new DB();
+    }
+    public static function getFilter($sql, $param){
+        $data = lcsDetallePerfilesModel::$DB->run($sql, $param);
+        return $data->fetchAll();
+    }
+
+    public static function getAll(){
+        lcsDetallePerfilesModel::conexionDB();
+        $sql = "Select * from perfiles";
+        $data = lcsDetallePerfilesModel::$DB->run($sql, []);
+        return $data->fetchAll();
+    }
+
+    public static function lcsnew($param){
+        try{
+             lcsPeriflesModel::conexionDB();
+             $sql = "insert into usuarios (usuarioid, nombre, apellidos, direccion, anioNac, ciudad) 
+                     values (?, ?, ?, ?, ?, ?)";
+             $data = lcsPerfilesModel::$DB->run($sql, $param);
+             return "perfil insertado correctamente ";
+        } 
+        catch(Exception $e){
+           return $e->getMessage();
+        }
+     }
+
+    public static function show($param){
+        lcsDetallePerfilesModel::conexionDB();
+        $sql = 'SELECT * from perfiles';
+        $data = lcsDetallePerfilesModel::$DB->run($sql, $param);
+        return $data->fetch();
+    }
+}
